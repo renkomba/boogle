36f599dd68f98ed7463a91acc1b2562e8a1c76a9
renkomba <61875726+renkomba@users.noreply.github.com>
2022-08-01 11:50:59 -0400

    created const dragNarration so the console can reflect if the draggable <Card /> is successfully manipulated

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,13 +7,36 @@ import { Content } from '../Content/Content';
 import { Footer } from '../Footer/Footer';
 import './App.css';
 
+const dragNarration = {
+  onDragStart(id) {
+    console.log(`Picked up the ${id} card`);
+  },
+  onDragOver(id, overId) {
+    if (overId) {
+      console.log(`The ${id} card was moved over the ${overId} area`);
+      return
+    }
+    console.log(`The ${id} card is no longer over a droppable area`);
+  },
+  onDragEnd(id, overId) {
+    if (overId) {
+      console.log(`The ${id} card was dropped over the ${overId} area`);
+    }
+    console.log(`The ${id} card was dropped`);
+  },
+  onDragCancel(id) {
+    console.log(`Dragging cancelled. The ${id} card was dropped`);
+  }
+};
+
 const App = () => {
   const [user, setUser] = useState(new Teacher());
-
   useEffect( 
     () => console.log(user), 
     [user]
   );
+
+  
 
   return (
     <div className="App">
