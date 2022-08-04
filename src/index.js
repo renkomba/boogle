33909e80b04ec5ea911744828bc587a24c6b2abd
renkomba <61875726+renkomba@users.noreply.github.com>
2022-08-04 03:02:40 -0400

    removed commented React-Dnd imports

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
-// import { DndProvider } from 'react-dnd';
-// import { HTML5Backend } from 'react-dnd-html5-backend';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
