ce0bbeca78b73f3e77817e68abcc4632a9c8ae30
renkomba <61875726+renkomba@users.noreply.github.com>
2022-08-04 02:49:05 -0400

    tweaked generatePeriods to make good use of the new Period class, no matter the toggled view

--- src/components/Content/Card/CardHeader/CardHeader.js
+++ src/components/Content/Card/CardHeader/CardHeader.js
@@ -4,13 +4,12 @@ import { PrepPeriod } from "./PrepPeriod/PrepPeriod";
 import './CardHeader.css';
 
 export const CardHeader = ({ prep, prepPeriods, minimised, setMinimised }) => {
-    // minimised = minimised === false ? setMinimised(true) : setMinimised(false);
     const generatePeriods = periods => {
-        return periods.map( period => <PrepPeriod 
-            key={`${period[0]}`} 
-            period={period === 'ct' ? period : period[0]}
-            minimised={minimised} 
-        />);
+        return periods.map( Period => <PrepPeriod 
+                key={`${Period.id}`} 
+                period={Period.period === 'ct' ? Period.period : Period.period[0]}
+                minimised={minimised} 
+            />);
     }
     
     return (
