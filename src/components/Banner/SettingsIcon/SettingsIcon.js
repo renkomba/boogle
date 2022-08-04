960da1df300e523a537cad41e63bc729aa2f85a7
renkomba <61875726+renkomba@users.noreply.github.com>
2022-08-04 02:43:20 -0400

    passed viewByPrep state and setter to toggle the prep view

--- src/components/Banner/SettingsIcon/SettingsIcon.js
+++ src/components/Banner/SettingsIcon/SettingsIcon.js
@@ -1,9 +1,12 @@
 import React from "react";
 import './SettingsIcon.css';
 
-export const SettingsIcon = () => {
+export const SettingsIcon = ({ viewByPrep, setViewByPrep }) => {
     return (
-        <section id="settings">
+        <section 
+            id="settings"
+            onClick={ () => setViewByPrep(!viewByPrep) }
+        >
             <i className="fa-solid fa-sliders"></i>
         </section>
     );
