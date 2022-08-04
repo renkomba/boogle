5bbeaa167d86a106e1d8b5470e8adc5d9556a137
renkomba <61875726+renkomba@users.noreply.github.com>
2022-08-04 02:54:04 -0400

    added rectSortingStrategy for dnd-kit for when the period-view cards become mini-cards down the road. Also beefed up generateCards() to handle both views. THe for loop remains mostly unchanged, but I added a final splice() at the end of the if statement to shorten the loop with each cardId

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -3,22 +3,31 @@ import { Card } from "./Card/Card";
 import { Sidebar } from "../Sidebar/Sidebar";
 import { useDroppable } from "@dnd-kit/core";
 import {
+    rectSortingStrategy,
     SortableContext,
     verticalListSortingStrategy
 } from "@dnd-kit/sortable";
 import './Content.css';
 
-export const Content = ({ id, cardIds, courses }) => {
+export const Content = ({ id, cardIds, user, viewByPrep }) => {
     const { setNodeRef } = useDroppable({ id });
    
     const generateCards = (cards=[]) => {
-        for (let cardId of cardIds) {
-            for (let course of courses) {
-                if (course.id === cardId) {
+        let [ ids, collective ] = [
+            cardIds.slice(0),
+            viewByPrep ? user.courses.slice(0) 
+                : [...user.periods.nonCts, user.periods.cts]
+        ];
+
+        for (let cardId of ids) {
+            for (let i = 0; i < collective.length; i++) {
+                if (collective[i].id === cardId) {
                     cards.push(<Card 
                         key={cardId}
-                        course={course} 
+                        course={collective[i]} 
+                        viewByPrep={viewByPrep}
                     />);
+                    collective.splice(i, 1);  //remove element as it is no longer an option
                 }
             }
         }
@@ -31,7 +40,10 @@ export const Content = ({ id, cardIds, courses }) => {
             <SortableContext 
                 id={id}
                 items={cardIds}
-                strategy={verticalListSortingStrategy}
+                strategy={
+                    viewByPrep ? verticalListSortingStrategy
+                        : rectSortingStrategy
+                }
             >
                 <article 
                     className="cards"
