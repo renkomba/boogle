1db4a441c37d196a9ab2ce0ed26c413e434ad64a
renkomba <61875726+renkomba@users.noreply.github.com>
2022-08-05 23:59:59 -0400

    fetch assignmetn class instance

--- src/classes/Course.js
+++ src/classes/Course.js
@@ -1,7 +1,8 @@
-import { getRandomNum } from "../hooks/getRandomNum";
-import { Period } from "./Period";
+import getRandomNum from "../components/functions/getRandomNum";
+import Assignment from "./Assignment";
+import Period from "./Period";
 
-export class Course {
+export default class Course {
     constructor(title='', periods=[]) {
         this.title = title;
         this.assignments = this.fetchAssignments();
@@ -28,50 +29,22 @@ export class Course {
     fetchAssignments() {
         const [ amount, assignments ] = [ getRandomNum(11, 3), {} ];
         for (let i = 0; i < amount; i++) {
-            let id = `${this.title.toLowerCase().split(' ').join('-')}-task-#${i+1}`;
-            assignments[id] = this.parseAssignment(id);
+            let [type, isCompleted] = this.parseAssignment();
+            assignments[id] = new Assignment(type, this, i+1, isCompleted);
         }
         return assignments
     }
 
-    parseAssignment(id='', assignmentId='test', type='', title='', submissions=0) {
-        const [ isTest, statuses, types, titles ] = [
+    parseAssignment(assignmentId='test') {
+        const [ isTest, statuses, types ] = [
             assignmentId === 'test' ? true : false,
             [ 'fully', 'partly', 'not'],
-            [
-                'assignment', 
-                'assessment', 
-                'application'
-            ],
-            {
-                assignment: [
-                    'TIL vocab drill', 'TIL structure practice', 
-                    'Conjuguemos conjugation', 'Conjuguemos grammar in context',
-                    'TV5 reading comprehension', 'TV5 listening comprehension',
-                    'Flipgrid speaking practice', 'Partner dialogue'
-                ],
-                assessment: [
-                    'Roleplay with Madame', 'Conversation Wizer',
-                    'Dictation', 'Unit essay',
-                    'Reading & Listening MCQ', 'Read and listen to act'
-                ],
-                application: [
-                    'Checkout conversation with Madame', 'Respond to a tweet',
-                    'Write a comment to YouTube video', 'Write an article review'
-                ]
-            }
+            [ 'assignment', 'assessment', 'application' ]
         ];
 
-        const completed = statuses[getRandomNum(statuses.length)];
-        submissions = !isTest ? submissions
-            : completed === 'partly' ? getRandomNum(this.totalStudents)
-            : completed === 'fully' ? this.totalStudents : 0;
+        const isCompleted = statuses[getRandomNum(statuses.length)];
+        const type = !isTest ? type : types[getRandomNum(types.length)];
 
-        type = !isTest ? type : types[getRandomNum(types.length)];
-
-        const validTitles = titles[type];
-        title = !isTest ? title : validTitles[getRandomNum(validTitles.length)]
-
-        return [id, type, title, submissions];
+        return [type, isCompleted];
     }
 }
