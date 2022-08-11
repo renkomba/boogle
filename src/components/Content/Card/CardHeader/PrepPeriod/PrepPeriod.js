39027eafdf33db4c80311fee669dd32433921b97
renkomba <61875726+renkomba@users.noreply.github.com>
2022-08-11 04:38:02 -0400

    reverted activePeriod back to pperiod because it isn't the active period but rather the period that matches the icon. Silly me.

--- src/components/Content/Card/CardHeader/PrepPeriod/PrepPeriod.js
+++ src/components/Content/Card/CardHeader/PrepPeriod/PrepPeriod.js
@@ -2,14 +2,14 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import './PrepPeriod.css';
 
-export const PrepPeriod = ({ periodId, activePeriod, minimised, setActivePeriod }) => {
+export const PrepPeriod = ({ periodId, period, minimised, setActivePeriod }) => {
     periodId = periodId === 'ct' ? 'people-group' : periodId;
 
     return (
         <Link to={'../../../../../pages/CourseSite'} className="Link">
             <i 
                 className={`prep-period ${minimised ? 'small ' : ''}fa-solid fa-${periodId}`}
-                onClick={ () => setActivePeriod(activePeriod) }
+                onClick={ () => setActivePeriod(period) }
             ></i>
         </Link>
     );
