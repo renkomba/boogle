2b16cd5420854a6e59ebb0596b44a118f87902f0
renkomba <61875726+renkomba@users.noreply.github.com>
2022-08-11 03:16:07 -0400

    created styled component for a group of assignments all under the same label. The assignments have different icons based on their type, resource type does not display turn-in ratio, and fully turned-in assignments display a check instead of the ratio

---
+++ src/components/Content/CourseContent/AssignmentGroup/AssignmentBar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import './AssignmentBar.css';
+
+const AssignmentBar = ({ icon, assignment, activePeriod }) => {
+    let submissionsObj = activePeriod.submissions[assignment.id]
+    return (
+        <div className="bar">
+            <i className={icon}></i>
+            <p className="title">{assignment.title}</p>
+            { 
+                assignment.type !== 'resource'
+                && (submissionsObj.isCompleted ?
+                <i className="fa-solid fa-check"></i>
+                : <p className="submissions">
+                    {submissionsObj.turnedIn}
+                    &nbsp;/&nbsp;
+                    {activePeriod.totalStudents}
+                </p>)
+            }
+        </div>
+    );
+}
+
+export default AssignmentBar;
---
+++ src/components/Content/CourseContent/AssignmentGroup/AssignmentGroup.js
@@ -0,0 +1,48 @@
+import React from "react";
+import AssignmentHeader from "./AssignmentHeader";
+import AssignmentBar from "./AssignmentBar";
+import './AssignmentGroup.css';
+
+const AssignmentGroup = ({ sectionLabel, assignmentsObj, activePeriod }) => {
+    const icons = {
+        assignment: 'fa-solid fa-file-lines',
+        assessment: 'fa-solid fa-file-lines',
+        application: 'fa-solid fa-file-lines',
+        resource: 'fa-regular fa-clipboard'
+    };
+
+    const populateAssignments = () => {
+        let assignmentBars = [];
+
+        for (let id in assignmentsObj) {
+            let assignment = assignmentsObj[id];
+            let icon = assignment.type + ' ' + icons[assignment.type];
+            console.log(sectionLabel);
+            
+            assignment.label === sectionLabel 
+                && assignmentBars.push( generateBar(icon, assignment) );
+        }
+
+        return assignmentBars;
+    };
+
+    const generateBar = (icon, assignment) => {
+        return (<AssignmentBar
+            key={assignment.id}
+            icon={icon}
+            assignment={assignment}
+            activePeriod={activePeriod}
+        />);
+    };
+
+    return (
+        <section className="group">
+            <AssignmentHeader sectionLabel={sectionLabel} />
+            <article className="bars">
+                {populateAssignments()}
+            </article>
+        </section>
+    );
+}
+
+export default AssignmentGroup;
