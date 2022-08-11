38c1da57e43e25d29729c4f3ed09f5b2f36615c5
renkomba <61875726+renkomba@users.noreply.github.com>
2022-08-11 04:55:44 -0400

    added 'resource' to parseAssignment() and limited the latter's purpose to simply returning an assignment type. Also simplified fetchAssignments and this.periods because the Period class now takes fewer parameters

--- src/classes/Course.js
+++ src/classes/Course.js
@@ -6,7 +6,10 @@ export default class Course {
     constructor(title='', periods=[]) {
         this.title = title;
         this.assignments = this.fetchAssignments();
-        this.periods = periods.map( period => new Period(title, this.id, period, this.assignments) );
+        this.periods = periods.map( 
+            period => new Period(title, this, period) 
+        );
+        
         this.totalStudents = this.periods.reduce( 
             (runningTotal, Period) => runningTotal + Period.totalStudents,
             0
@@ -29,22 +32,16 @@ export default class Course {
     fetchAssignments() {
         const [ amount, assignments ] = [ getRandomNum(11, 3), {} ];
         for (let i = 0; i < amount; i++) {
-            let [type, isCompleted] = this.parseAssignment();
-            assignments[id] = new Assignment(type, this, i+1, isCompleted);
+            let type = this.parseAssignment();
+            let id = `${this.id}-task-#${i + 1}`;
+            assignments[id] = new Assignment(type, this, i+1);
         }
         return assignments
     }
 
-    parseAssignment(assignmentId='test') {
-        const [ isTest, statuses, types ] = [
-            assignmentId === 'test' ? true : false,
-            [ 'fully', 'partly', 'not'],
-            [ 'assignment', 'assessment', 'application' ]
-        ];
-
-        const isCompleted = statuses[getRandomNum(statuses.length)];
-        const type = !isTest ? type : types[getRandomNum(types.length)];
-
-        return [type, isCompleted];
+    parseAssignment() {
+        const types = [ 'assignment', 'assessment', 'application', 'resource' ];
+        const type = types[getRandomNum(types.length)];
+        return type;
     }
 }
