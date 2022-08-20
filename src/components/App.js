bcd2e1c83602dfadc4ff91cee5dc035481244a47
renkomba <61875726+renkomba@users.noreply.github.com>
2022-08-19 23:51:35 -0400

    moved the heavy lifting to the Main component

--- src/components/App.js
+++ src/components/App.js
@@ -1,67 +1,28 @@
 // import { render } from '@testing-library/react';
 import React, { useState } from 'react';
 import Teacher from '../classes/Teacher';
-import Dashboard from './pages/Dashboard';
-import CourseSite from './pages/CourseSite';
-import Banner from './Banner/Banner';
-import Footer from './Footer/Footer';
-import './App.css';
-import { Outlet } from 'react-router-dom';
-import { Routes, Route } from 'react-router-dom';
-import PeriodBanner from './Banner/PeriodBanner/PeriodBanner';
+import Main from './Main/Main';
+
+// set variable if undefined: let var 
+// A. var ??= newVal;
+// B. var = var ?? newVal
 
 const App = () => {
   const [user, setUser] = useState(new Teacher());
-  const [toggle, setToggle] = useState(true);
-  const [activePeriod, setActivePeriod] = useState(user.periods.nonCts[0]);
   const [activePage, setActivePage] = useState('Dashboard');
+
+  const changeUser = () => setUser(new Teacher());
+  const changePage = () => setActivePage(
+    activePage === 'Dashboard' ? 'Course Site' : 'Dashboard'
+  );
 
   return (
     <div className="App">
-      { activePage === 'Course Site' ?
-        <PeriodBanner 
-          periods={user.periods}
-          activePeriod={activePeriod}
-          setActivePeriod={setActivePeriod}
-          setActivePage={setActivePage}
-        /> : <Banner 
-          user={user}
-          setUser={setUser}
-          toggle={toggle}
-          setToggle={setToggle}
-        />
-      }
-
-      <Routes>
-        <Route 
-          index 
-          element={<Dashboard 
-            user={user}
-            toggle={toggle}
-            activePage={activePage}
-            setActivePage={setActivePage}
-            activePeriod={activePeriod}
-            setActivePeriod={setActivePeriod}
-            />} 
-            />
-        <Route
-          path='pages/CourseSite' 
-          element={<CourseSite 
-            user={user}
-            toggle={toggle}
-            activePeriod={activePeriod}
-            activePage={activePage}
-            setActivePage={setActivePage}
-          />} 
-        />
-      </Routes>
-
-      <Outlet 
-        user={user}
-        toggle={toggle}
+      <Main user={user}
+        activePage={activePage}
+        changePage={changePage}
+        changeUser={changeUser}
       />
-
-      <Footer />
     </div>
   );
 }
