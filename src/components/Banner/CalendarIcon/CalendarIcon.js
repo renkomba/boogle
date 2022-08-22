c66745aa3381640c18baf023225e2e8beaedfe61
renkomba <61875726+renkomba@users.noreply.github.com>
2022-08-22 12:16:38 -0400

    switched to a default export, and made code more readable

--- src/components/Banner/CalendarIcon/CalendarIcon.js
+++ src/components/Banner/CalendarIcon/CalendarIcon.js
@@ -2,14 +2,18 @@ import React from "react";
 import getOrdinalSuffix from "../../functions/getOrdinalSuffix";
 import './CalendarIcon.css';
 
-export const CalendarIcon = () => {
+const CalendarIcon = () => {
     let [ today, months, days ] = [
         new Date(),
         [
-            'January', 'February', 'March', 'April', 'May', 'June', 
-            'July', 'August', 'September', 'October', 'November', 'December'
+            'January', 'February', 'March', 'April', 
+            'May', 'June', 'July', 'August', 
+            'September', 'October', 'November', 'December'
         ],
-        ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+        [
+            'Monday', 'Tuesday', 'Wednesday', 'Thursday', 
+            'Friday', 'Saturday', 'Sunday'
+        ]
     ];
 
     let [ day, month, weekday] = [
@@ -22,11 +26,17 @@ export const CalendarIcon = () => {
 
     return (
         <section id="date">
-            <div id="date-container" title={`Today is ${weekday}, ${month} the ${day}${suffix}`}>
-                <span id="month">{month.length > 4 ? `${month.slice(0, 3)}.` : month}</span><br/>
+            <div id="date-container" 
+                title={`Today is ${weekday}, ${month} the ${day}${suffix}`}
+            >
+                <span id="month">
+                    {month.length > 4 ? `${month.slice(0, 3)}.` : month}
+                </span><br/>
                 <span id="day">{day}</span>
                 <span id="ordinal-suffix"><sup>{suffix}</sup></span>
             </div>
         </section>
     );
 }
+
+export default CalendarIcon;
