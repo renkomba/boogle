fff922b87f6a304b0c28e1a5d3021702cc9268f9
renkomba <61875726+renkomba@users.noreply.github.com>
2022-08-23 22:34:03 -0400

    passed filterByTag() as prop to <Tags />

--- src/components/Sidebar/CourseSidebar.js
+++ src/components/Sidebar/CourseSidebar.js
@@ -3,11 +3,13 @@ import Periods from "../CourseContent/Tags/Periods";
 import Tags from "../CourseContent/Tags/Tags";
 import styles from './CourseSidebar.module.css';
 
-const CourseSidebar = ({ activePeriod }) => {
+const CourseSidebar = ({ activePeriod, filterByTag }) => {
     return (
         <aside className={styles.course_sidebar}>
             <div>
-                <Tags activePeriod={activePeriod} />
+                <Tags activePeriod={activePeriod} 
+                    filterByTag={filterByTag}
+                />
                 <br />
                 <Periods activePeriod={activePeriod} />
             </div>
