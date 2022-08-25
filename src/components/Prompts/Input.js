ab224f6758c2c2759cb0c4f07ba2b7b5a342a411
renkomba <61875726+renkomba@users.noreply.github.com>
2022-08-25 12:07:10 -0400

    changed <Form /> wrapper to <Form.Group /> because it appears as a child of other <Form /> components, and added classNames for styling

--- src/components/Prompts/Input.js
+++ src/components/Prompts/Input.js
@@ -7,23 +7,23 @@ import styles from './Input.module.css';
 const Input = ({ contentType, inputType, handleToggle }) => {
     // console.log(`Type: ${contentType}\nInput Type: ${inputType}`);
     return (
-        <Form>
+        <Form.Group className={styles.input_grid}>
             <FloatingLabel
                 controlId="quickInput"
-                label={`New ${contentType[0].toUpperCase() + contentType.slice(1)}`}
-                className={styles.inputPrompt}
                 onChange={handleToggle}
+                className={styles.input_prompt}
                 onKeyDown={ e => e.key === 'Enter' && handleToggle}
+                label={`${contentType[0].toUpperCase() + contentType.slice(1)}`}
             >
-                <Form.Control 
-                    type={inputType}
+                <Form.Control type={inputType}
+                    className={styles.add_input}
                     min={inputType === 'number' ? 1 : ''}
                 ></Form.Control>
             </FloatingLabel>
-            <Button type='submit'>
+            <Button type='submit' className={styles.submit_input}>
                 <i className="fa-solid fa-plus"></i>
             </Button>
-        </Form>
+        </Form.Group>
     );
 }
 
