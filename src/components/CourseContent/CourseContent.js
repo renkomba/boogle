79bf53cc946ba0867e756a6af3e48d16ae7383bf
renkomba <61875726+renkomba@users.noreply.github.com>
2022-08-26 20:42:32 -0400

    wrote function to process <AssignmentModal /> data

--- src/components/CourseContent/CourseContent.js
+++ src/components/CourseContent/CourseContent.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import AssignmentGroup from "./AssignmentGroup/AssignmentGroup";
 import styles from './CourseContent.module.css';
 import { 
@@ -21,6 +21,7 @@ import AssignmentOverlay from "../Overlays/AssignmentOverlay";
 import CategoryOverlay from "../Overlays/CategoryOverlay";
 import CourseSidebar from "../Sidebar/CourseSidebar";
 import AddButtons from "../Buttons/AddButtons";
+import UserContext from "../../contexts/userContext";
 
 const CourseContent = ({ activePeriod, id }) => {
     const [tags, setTags] = useState( activePeriod.course ?
@@ -46,6 +47,8 @@ const CourseContent = ({ activePeriod, id }) => {
             ])
         )
     );
+
+    const { user, currentCourse } = useContext(UserContext);
 
     useEffect(
         () => {
@@ -264,6 +267,47 @@ const CourseContent = ({ activePeriod, id }) => {
         setActiveIds( activeIds => ({...activeIds, bar: null}) );
     }
 
+    const addAssignmentToGroup = (values=[]) => {
+        let currentCourseAssignments = Object.keys(currentCourse.assignments);
+        let assignmentKey = currentCourseAssignments[currentCourseAssignments.length - 1];
+        let assignment = currentCourse.assignments[assignmentKey];
+        
+        let periods = values.slice(0);
+        [
+            assignment.title, 
+            assignment.type, 
+            assignment.label, 
+            assignment.directions
+        ] = periods.splice(0, 4);
+        assignment.attachments = periods.pop();
+
+        for (let id of periods) {
+            id = id[0].toUpperCase() + id.slice(1);
+            let isCourse = !id.includes('-');
+            let course = isCourse ? user.courses.find(
+                Course => Course.title === id
+            ) : user.courses.find(
+                Course => Course.id === id.split('-').slice(0, 2).join('-')
+            );
+
+            if (course.title === currentCourse.title) {
+                setAssignmentGroups(Object.fromEntries(
+                    (activePeriod.course ? activePeriod.course 
+                    : activePeriod).assignmentLabels.map( tag => [
+                        tag,
+                        Object.values(activePeriod.assignments).filter(
+                            Assignment => Assignment.label === tag
+                        ).map( Assignment => Assignment.id )
+                    ])
+                ));
+                setTags(activePeriod.course ?
+                    activePeriod.course.assignmentLabels 
+                    : activePeriod.assignmentLabels
+                );
+            }
+        }
+    }
+
     return (
         <div className={styles.course_content}>
             <CourseSidebar activePeriod={activePeriod}
@@ -314,9 +358,8 @@ const CourseContent = ({ activePeriod, id }) => {
                     </DndContext>
                 </section>
                 <AddButtons activePeriod={activePeriod} 
-                    setAssignmentGroups={setAssignmentGroups}
+                    addAssignmentToGroup={addAssignmentToGroup}
                 />
-                {}
             </div>
 
         </div>
