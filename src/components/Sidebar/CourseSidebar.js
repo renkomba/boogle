be4a931e4759c94fdbbfa326526cd29d830c473e
renkomba <61875726+renkomba@users.noreply.github.com>
2022-08-28 23:52:29 -0400

    moved tag state up into <CourseContent />, where its changes can affect activePeriod state to facilitate an automatic refresh (in future)

--- src/components/Sidebar/CourseSidebar.js
+++ src/components/Sidebar/CourseSidebar.js
@@ -3,12 +3,14 @@ import Periods from "../CourseContent/Tags/Periods";
 import Tags from "../CourseContent/Tags/Tags";
 import styles from './CourseSidebar.module.css';
 
-const CourseSidebar = ({ activePeriod, filterByTag }) => {
+const CourseSidebar = ({ activePeriod, filterByTag, setTags, tags }) => {
     return (
         <aside className={styles.course_sidebar}>
             <div>
                 <Tags activePeriod={activePeriod} 
                     filterByTag={filterByTag}
+                    setTags={setTags}
+                    tags={tags}
                 />
                 <br />
                 <Periods activePeriod={activePeriod} />
